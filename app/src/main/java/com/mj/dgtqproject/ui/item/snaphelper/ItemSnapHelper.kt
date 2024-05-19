84a4c80252bfde457c9423db14ba18f5ae05677d
Franqu <maciejedrzejko@gmail.com>
2024-05-19 13:02:52 +0200

    overrided findTargetSnapPosition to span by page size where page size is columnCount * rowCount

--- app/src/main/java/com/mj/dgtqproject/ui/item/snaphelper/ItemSnapHelper.kt
+++ app/src/main/java/com/mj/dgtqproject/ui/item/snaphelper/ItemSnapHelper.kt
@@ -1,17 +1,45 @@
 package com.mj.dgtqproject.ui.item.snaphelper
 
+import android.view.View
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 
-class ItemSnapHelper(pageSize : Int) : LinearSnapHelper() {
-
-    private val pageSize = pageSize
+class ItemSnapHelper(private val pageSize: Int) : LinearSnapHelper() {
 
     override fun findTargetSnapPosition(
-        layoutManager: RecyclerView.LayoutManager?,
+        layoutManager: RecyclerView.LayoutManager,
         velocityX: Int,
         velocityY: Int
     ): Int {
-        return super.findTargetSnapPosition(layoutManager, velocityX, velocityY)
+        val centerView = findSnapView(layoutManager) ?: return RecyclerView.NO_POSITION
+        val position = layoutManager.getPosition(centerView)
+        var targetPosition = -1
+        if (layoutManager.canScrollHorizontally()) {
+            targetPosition = if (velocityX < 0) {
+                position - pageSize
+            } else {
+                position + pageSize
+            }
+        }
+
+        if (layoutManager.canScrollVertically()) {
+            targetPosition = if (velocityY < 0) {
+                position - pageSize
+            } else {
+                position + pageSize
+            }
+        }
+
+        val firstItem = 0
+        val lastItem = layoutManager.itemCount - 1
+        targetPosition = Math.min(lastItem, Math.max(targetPosition, firstItem))
+        return targetPosition
+    }
+
+    override fun calculateDistanceToFinalSnap(
+        layoutManager: RecyclerView.LayoutManager,
+        targetView: View
+    ): IntArray? {
+        return super.calculateDistanceToFinalSnap(layoutManager, targetView)
     }
 }
